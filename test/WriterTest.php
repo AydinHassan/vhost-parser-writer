14136409f18a7abbf8b0002b86a6dad8f96d7605
Aydin <aydin@wearejh.com>
2014-10-13 18:03:07 +0100

    Function to strip idents

--- test/WriterTest.php
+++ test/WriterTest.php
@@ -24,4 +24,44 @@ class WriterTest extends \PHPUnit_Framework_TestCase
         $expected = "<VirtualHost 212.34.23.123:200>\n\n</VirtualHost>";
         $this->assertSame($expected, $this->vHostWriter->write());
     }
+
+    public function testVhostWriteWithParams()
+    {
+        $expected = $this->stripIndent('
+        <VirtualHost 212.34.23.123:200>
+
+        </VirtualHost>
+        ');
+
+        $this->assertSame($expected, $this->vHostWriter->write());
+    }
+
+
+    public function stripIndent($multiLineString)
+    {
+        $lines = explode("\n", $multiLineString);
+
+        if (count($lines) < 1) {
+            return $multiLineString;
+        }
+
+        $firstLine  = $lines[1];
+        $matches    = [];
+        preg_match('/\S/', $firstLine, $matches, PREG_OFFSET_CAPTURE);
+        $whiteSpaceCount = $matches[0][1];
+
+        $strippedLines = [];
+        for ($i = 0; $i < count($lines); $i++) {
+
+            if ($i === 0) {
+                continue;
+            }
+
+            $line               = $lines[$i];
+            $strippedLines[]    = substr($line, $whiteSpaceCount);
+        }
+
+        $return = rtrim(implode("\n", $strippedLines), "\n");
+        return $return;
+    }
 }
